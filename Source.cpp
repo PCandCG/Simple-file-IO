6702a2eb4e6c7826675170e5235c7d75bc93f390
PCandCG <37952236+PCandCG@users.noreply.github.com>
2018-08-19 11:14:29 +0100

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -8,10 +8,6 @@ int main() {
 	string inputText;
 	cin >> inputText;
 	cout << inputText;
-	/*ofstream myfile;
-	myfile.open("Output.txt");
-	myfile << inputText;
-	myfile.close();*/
 	ofstream file(path); //open in constructor
 	file << inputText;
 	system("PAUSE");
